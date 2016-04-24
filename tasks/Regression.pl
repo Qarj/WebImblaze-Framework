a9273ebe2a71c712c3b94b375ed2dab6c325ba9a
Tim Buckland <tim.buckland@totaljobsgroup.com>
2016-04-24 16:18:48 +0100

    separated out re-used functions into Runner.pm

--- tasks/Regression.pl
+++ tasks/Regression.pl
@@ -8,91 +8,33 @@ use strict;
 use warnings;
 use vars qw/ $VERSION /;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
-use Cwd;
-use Config::Tiny;
-use File::Spec;
+require Runner;
 
 local $| = 1; # don't buffer output to STDOUT
 
-my ($config_target, $config_batch, $config_environment, $config_wif_location);
-
-read_config();
+my ($config_target, $config_batch, $config_environment, $config_wif_location) = Runner::read_config('Regression.config');
 
 # add a random number to the batch name so this run will have a different name to a previous run
-my $_random = int rand 99_999;
-$_random = sprintf '%05d', $_random; # add some leading zeros
-$config_batch .= q{_}.$_random;
+$config_batch .= Runner::random(99_999);
 
 # specify the location of the test files relative to this script
-start_test('../../WebInject/examples/command.xml');
-start_test('../../WebInject/examples/command.xml');
+start('../../WebInject/examples/command.xml');
+start('../../WebInject/examples/command.xml');
+start('../../WebInject/examples/command.xml');
+start('../../WebInject/examples/command.xml');
+start('../../WebInject/examples/command.xml');
+start('../../WebInject/examples/command.xml');
+start('../../WebInject/examples/command.xml');
+start('../../WebInject/examples/command.xml');
+start('../../WebInject/examples/command.xml');
 
-sub start_test {
-    my ($_test_file_full) = @_;
 
-    my $_abs_test_file_full = File::Spec->rel2abs( $_test_file_full );
+sub start {
+    my ($_test) = @_;
 
-    my @_args;
-
-    push @_args, $_abs_test_file_full;
-
-    push @_args, '--target';
-    push @_args, $config_target;
-
-    push @_args, '--batch';
-    push @_args, $config_batch;
-
-    push @_args, '--env';
-    push @_args, $config_environment;
-
-    push @_args, '--use-browsermob-proxy';
-    push @_args, 'false';
-
-    push @_args, '--no-update-config';
-
-    # wif.pl expects the current working directory to be where wif.pl is located
-    my $_orig_cwd = cwd;
-    chdir $config_wif_location;
-
-    _start_windows_process('wif.pl '."@_args");
-
-    chdir $_orig_cwd;
-
-    return;
-}
-
-#------------------------------------------------------------------
-sub _start_windows_process {
-    my ($_command) = @_;
-
-    my $_cwd = cwd;
-    my $_wmic = "wmic process call create 'cmd /c cd $_cwd & $_command'"; #
-
-    my $_result = `$_wmic`;
-    #print "_wmic:$_wmic\n";
-    #print "$_result\n";
-
-    my $_pid;
-    if ( $_result =~ m/ProcessId = (\d+)/ ) {
-        $_pid = $1;
-    }
-
-    return $_pid;
-}
-
-#------------------------------------------------------------------
-sub read_config {
-    my $_config = Config::Tiny->read( 'Regression.config' );
-
-    # main
-    $config_target = $_config->{main}->{target};
-    $config_batch = $_config->{main}->{batch};
-    $config_environment = $_config->{main}->{environment};
-
-    # path
-    $config_wif_location = $_config->{path}->{wif_location};
+    Runner::start_test($_test, $config_target, $config_batch, $config_environment, $config_wif_location);
 
     return;
 }
---
+++ tasks/Runner.pm
@@ -0,0 +1,100 @@
+#!/usr/bin/perl
+
+# $Id$
+# $Revision$
+# $Date$
+
+package Runner;
+
+use strict;
+use warnings;
+use vars qw/ $VERSION /;
+
+$VERSION = '0.01';
+
+use Cwd;
+use Config::Tiny;
+use File::Spec;
+
+sub start_test {
+    my ($_test_file_full, $_config_target, $_config_batch, $_config_environment, $_config_wif_location) = @_;
+
+    my $_abs_test_file_full = File::Spec->rel2abs( $_test_file_full );
+
+    my @_args;
+
+    push @_args, $_abs_test_file_full;
+
+    push @_args, '--target';
+    push @_args, $_config_target;
+
+    push @_args, '--batch';
+    push @_args, $_config_batch;
+
+    push @_args, '--env';
+    push @_args, $_config_environment;
+
+    push @_args, '--use-browsermob-proxy';
+    push @_args, 'false';
+
+    push @_args, '--no-update-config';
+
+    # wif.pl expects the current working directory to be where wif.pl is located
+    my $_orig_cwd = cwd;
+    chdir $_config_wif_location;
+
+    _start_windows_process('wif.pl '."@_args");
+
+    chdir $_orig_cwd;
+
+    return;
+}
+
+#------------------------------------------------------------------
+sub _start_windows_process {
+    my ($_command) = @_;
+
+    my $_cwd = cwd;
+    my $_wmic = "wmic process call create 'cmd /c cd $_cwd & $_command'"; #
+
+    my $_result;
+    $_result = `$_wmic`;
+    #print "_wmic:$_wmic\n";
+    #print "$_result\n";
+
+    my $_pid;
+    if ( $_result =~ m/ProcessId = (\d+)/ ) {
+        $_pid = $1;
+    }
+
+    return $_pid;
+}
+
+#------------------------------------------------------------------
+sub random {
+    my ($_max) = @_;
+
+    my $_random = int rand $_max;
+    $_random = sprintf '%05d', $_random; # add some leading zeros
+
+    return $_random;
+}
+
+#------------------------------------------------------------------
+sub read_config {
+    my ($_config_file_full) = @_;
+
+    my $_config = Config::Tiny->read( $_config_file_full );
+
+    # main
+    my $_config_target = $_config->{main}->{target};
+    my $_config_batch = $_config->{main}->{batch};
+    my $_config_environment = $_config->{main}->{environment};
+
+    # path
+    my $_config_wif_location = $_config->{path}->{wif_location};
+
+    return ($_config_target, $_config_batch, $_config_environment, $_config_wif_location);
+}
+
+1;
