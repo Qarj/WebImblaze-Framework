e5e331619087681b81db2e14f83369e60f35dec1
Tim Buckland <tim.buckland@totaljobsgroup.com>
2017-06-04 15:33:04 +0100

    example shows how a session cookie can be shared among concurrent executions of a script

---
+++ tasks/SyncExample.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+# $Id$
+# $Revision$
+# $Date$
+
+use strict;
+use vars qw/ $VERSION /;
+
+use File::Basename;
+chdir dirname(__FILE__);
+require Runner;
+
+$VERSION = '1.04';
+
+## Usage:
+##
+## tasks\<task_filename>.pl --env PAT --target bcs --batch Core_Regression --check-alive http://www.example.com
+
+Runner::start_runner();
+
+# specify the location of the test files relative to this script
+Runner::start('../../WebInject/examples/myaccount.xml');
+Runner::start('../../WebInject/examples/myaccount.xml');
+Runner::start('../../WebInject/examples/myaccount.xml');
+Runner::start('../../WebInject/examples/myaccount.xml');
+Runner::start('../../WebInject/examples/myaccount.xml');
+
+
+Runner::stop_runner();
