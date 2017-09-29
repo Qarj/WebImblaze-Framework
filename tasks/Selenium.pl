95df2f7c6367be0e1c63df16d0fa91820af58053
Tim Buckland <tim.buckland@totaljobsgroup.com>
2017-09-29 16:17:44 +0100

    additional examples added to Selenium task

--- tasks/Selenium.pl
+++ tasks/Selenium.pl
@@ -24,13 +24,21 @@ my $update = system('..\..\WebInject-Selenium\plugins\update.pl');
 
 # specify the location of the test files relative to this script
 Runner::start('../../WebInject-Selenium/examples/Caterer.xml');
+Runner::start('../../WebInject-Selenium/examples/Condobolin.xml');
+Runner::start('../../WebInject-Selenium/examples/food.gov.uk.xml');
 Runner::start('../../WebInject-Selenium/examples/govuk.xml');
+Runner::start('../../WebInject-Selenium/examples/graincorp.xml');
+Runner::start('../../WebInject-Selenium/examples/gumtree.xml');
 Runner::start('../../WebInject-Selenium/examples/Jobsite.xml');
 Runner::start('../../WebInject-Selenium/examples/NHSJobs.xml');
+Runner::start('../../WebInject-Selenium/examples/panerabread.xml');
+Runner::start('../../WebInject-Selenium/examples/rightmove.xml');
 Runner::start('../../WebInject-Selenium/examples/searchimage.xml');
 Runner::start('../../WebInject-Selenium/examples/selenium.xml');
 Runner::start('../../WebInject-Selenium/examples/StepStone.xml');
 Runner::start('../../WebInject-Selenium/examples/test_jobs.xml');
+Runner::start('../../WebInject-Selenium/examples/trainline.xml');
+Runner::start('../../WebInject-Selenium/examples/tripadvisor.xml');
 Runner::start('../../WebInject-Selenium/examples/webinject-check.xml');
 
 # start the Selenium based tests first since they are slower
