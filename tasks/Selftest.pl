76b4566dcf0e81a5542df1f039026ca00520cfa8
Tim Buckland <tim.buckland@totaljobsgroup.com>
2017-10-12 18:52:25 +0100

    added SSL self test

--- tasks/Selftest.pl
+++ tasks/Selftest.pl
@@ -70,6 +70,7 @@ Runner::start('../../WebInject/selftest/runif.xml');
 Runner::start('../../WebInject/selftest/runon.xml');
 Runner::start('../../WebInject/selftest/section.xml');
 Runner::start('../../WebInject/selftest/setcookie.xml');
+Runner::start('../../WebInject/selftest/ssl.xml');
 Runner::start('../../WebInject/selftest/sharedvar.xml');
 Runner::start('../../WebInject/selftest/specialcharacters.xml');
 Runner::start('../../WebInject/selftest/substitutions.xml');
