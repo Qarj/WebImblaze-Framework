6ce6006edf0e0b6f3810c6c66d0b80dd5695e3a0
Qarj <tim.buckland@totaljobsgroup.com>
2018-03-25 12:09:26 +0100

    run the Selenium examples less aggressively

--- tasks/Selenium.pl
+++ tasks/Selenium.pl
@@ -28,23 +28,25 @@ Runner::start('../../WebInject-Selenium/examples/Caterer.xml');
 Runner::start('../../WebInject-Selenium/examples/Condobolin.xml');
 Runner::start('../../WebInject-Selenium/examples/food.gov.uk.xml');
 Runner::start('../../WebInject-Selenium/examples/govuk.xml');
-Runner::start('../../WebInject-Selenium/examples/graincorp.xml');
+Runner::call('../../WebInject-Selenium/examples/graincorp.xml');
+
 Runner::start('../../WebInject-Selenium/examples/gumtree.xml');
 Runner::start('../../WebInject-Selenium/examples/Jobsite.xml');
 Runner::start('../../WebInject-Selenium/examples/NHSJobs.xml');
 Runner::start('../../WebInject-Selenium/examples/rightmove.xml');
-Runner::start('../../WebInject-Selenium/examples/searchimage.xml');
+Runner::call('../../WebInject-Selenium/examples/searchimage.xml');
+
 Runner::start('../../WebInject-Selenium/examples/selenium.xml');
 Runner::start('../../WebInject-Selenium/examples/StepStone.xml');
 Runner::start('../../WebInject-Selenium/examples/test_jobs.xml');
 Runner::start('../../WebInject-Selenium/examples/trainline.xml');
-Runner::start('../../WebInject-Selenium/examples/tripadvisor.xml');
-Runner::start('../../WebInject-Selenium/examples/webinject-check.xml');
+Runner::call('../../WebInject-Selenium/examples/tripadvisor.xml');
 
-# start the Selenium based tests first since they are slower
+Runner::start('../../WebInject-Selenium/examples/webinject-check.xml');
 Runner::start('../../WebInject-Selenium/selftest/substeps/_click.xml');
 Runner::start('../../WebInject-Selenium/selftest/substeps/_keys_to_element.xml');
-Runner::start('../../WebInject-Selenium/selftest/substeps/_move_to.xml');
+Runner::call('../../WebInject-Selenium/selftest/substeps/_move_to.xml');
+
 Runner::start('../../WebInject-Selenium/selftest/substeps/_wait_visible.xml');
 Runner::start('../../WebInject-Selenium/selftest/selenium_core.xml');
 
